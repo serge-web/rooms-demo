95c78f52cdcaea97f3d519138e4a7f632b65d827
Ian Mayo <ian@planetmayo.com>
2024-10-29 14:38:16 +0000

    ISO 8601 validation

--- src/admin/resources/states/StateEdit.tsx
+++ src/admin/resources/states/StateEdit.tsx
@@ -1,14 +1,17 @@
-import { DateTimeInput, Edit, NumberInput, SimpleForm, TextInput } from 'react-admin';
+import { DateTimeInput, Edit, NumberInput, required, regex, SimpleForm, TextInput } from 'react-admin';
+
+
+const validateDuration = [required(),  regex(/^(-?)P(?=\d|T\d)(?:(\d+)Y)?(?:(\d+)M)?(?:(\d+)([DW]))?(?:T(?:(\d+)H)?(?:(\d+)M)?(?:(\d+(?:\.\d+)?)S)?)?$/, 'Must be valid ISO 8601 Duration')];
 
 export const StateEdit = () => (
     <Edit>
-        <SimpleForm>
-            <TextInput source="id" />
+        <SimpleForm  mode="onBlur" reValidateMode="onBlur">
+            <TextInput disabled source="id" />
             <TextInput source="description" />
-            <NumberInput source="turn" />
-            <DateTimeInput source="gameDate" />
-            <TextInput source="turnTime" />
-            <TextInput source="planningAllowance" />
+            <NumberInput source="turn" validate={required()} />
+            <DateTimeInput source="gameDate" validate={required()}/>
+            <TextInput source="turnTime" validate={validateDuration}/>
+            <TextInput source="planningAllowance"  validate={validateDuration}/>
         </SimpleForm>
     </Edit>
 );
