7c1a14c772d5304866db741a4a3b3be0ef7c7e1f
Ian Mayo <ian@planetmayo.com>
2024-10-29 17:25:06 +0000

    force color picker

--- src/admin/helpers/ColorField.tsx
+++ src/admin/helpers/ColorField.tsx
@@ -2,13 +2,13 @@ import { useRecordContext } from "react-admin";
 
 // regex for an html color code
 export const htmlRegEx = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
+
 const backColor = '#ccc';
 
 export const ColorField = ({ source }: { source: string }) => {
   const record = useRecordContext();
   if (!record) return null;
   const rgb = record[source];
-  console.log('color: ', rgb);
   // use regex to test if the string is a valid rgb color
   if (htmlRegEx.test(rgb)) {
     return <div style={{ alignContent:'center', backgroundColor:backColor, color: rgb }}>{rgb}</div>;
--- src/admin/resources/forces/ForcesEdit.tsx
+++ src/admin/resources/forces/ForcesEdit.tsx
@@ -1,14 +1,12 @@
-import { Edit, regex, SimpleForm, TextInput } from 'react-admin';
-import { htmlRegEx } from '../../helpers/ColorField';
-
-const validateColor = regex(htmlRegEx, 'Invalid color code (expecting #f00, #226644, etc.)');
+import { Edit, SimpleForm, TextInput } from 'react-admin';
+import ColorPicker from '../../helpers/ColorPicker';
 
 export const ForceEdit = () => (
     <Edit>
         <SimpleForm mode="onBlur" reValidateMode="onBlur">
             <TextInput disabled source="id" />
             <TextInput source="name" />
-            <TextInput source="color" validate={validateColor} />
+            <ColorPicker source="color" label="Color"/>
             <TextInput multiline minRows={5} source="objective" />
         </SimpleForm>
     </Edit>
