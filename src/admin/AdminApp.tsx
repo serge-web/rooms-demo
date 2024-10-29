d53733f5fe7da1b6ede85b0953c8fcb14a15eaea
Ian Mayo <ian@planetmayo.com>
2024-10-29 13:27:42 +0000

    Provide resource names and icons

--- src/admin/AdminApp.tsx
+++ src/admin/AdminApp.tsx
@@ -5,6 +5,7 @@ import {
   EditGuesser,
   ShowGuesser,
 } from "react-admin";
+import { Description, EventAvailable, Groups, MeetingRoom, Person, RoomPreferences } from "@mui/icons-material";
 import { Layout } from "./Layout.tsx";
 import localDataProvider from "./localDataProvider.ts";
 import { authProvider } from "./authProvider.ts";
@@ -24,39 +25,47 @@ export const AdminApp = () => (
   >
     <Resource
       name="states"
+      options={{ label: 'Current game state' }} 
+      icon={EventAvailable}
       list={ListGuesser}
       edit={EditGuesser}
       show={ShowGuesser}
     />
     <Resource
       name="forces"
+      icon={Groups}
       list={ListGuesser}
       edit={EditGuesser}
       show={ShowGuesser}
     />
     <Resource
       name="users"
+      icon={Person}
       list={ListGuesser}
       edit={EditGuesser}
       show={ShowGuesser}
     />
     <Resource
       name="rooms"
+      icon={MeetingRoom}
       list={ListGuesser}
       edit={EditGuesser}
       show={RoomShow}
     />
     <Resource
       name="roomParticipations"
+      options={{ label: 'Room Membership' }} 
+      icon={RoomPreferences}
       list={RoomParticipationList}
       edit={RoomParticipationEdit}
       show={RoomParticipationShow}
     />
     <Resource
       name="templates"
+      icon={Description}
       list={ListGuesser}
       edit={EditGuesser}
-      show={RoomShow}
+      show={ShowGuesser}
     />
   </Admin>
 );
